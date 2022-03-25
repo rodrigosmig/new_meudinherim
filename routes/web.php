e6440cf0df503e3ecb32394c50d3b7bf79f96ac8
Rodrigo Miguel <rodrigosmig@gmail.com>
2022-03-24 21:18:09 -0300

    Disable web requests

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'DashboardController@home');
+/* Route::get('/', 'DashboardController@home');
 
 Auth::routes();
 
@@ -91,4 +92,8 @@ Route::group([
     //Notifications
     Route::get('notifications', 'NotificationController@all_read')->name('notifications.all_read');
     Route::get('notifications/{notification_id}/account/{account_id}', 'NotificationController@show')->name('notifications.as_read');
+}); */
+
+Route::fallback(function () {
+    return Redirect::away(config('app.url'));
 });
